137f1831806b7034cb54712fdc218b9564b21cce
Awais Qurashi <awaisqurashi43@gmail.com>
2023-01-26 14:35:22 +0500

    final

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <div><nav className="navbar navbar-expand-lg navbar-dark bg-dark">
     <div className="container-fluid">
-      <a className="navbar-brand" href="#">reactApp</a>
+      <a className="navbar-brand" href="https://github.com/awaisqurashi/react-router-demo">reactApp</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
@@ -29,7 +29,7 @@ function Navbar() {
             </ul>
           </li>
           <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            <a class="nav-link disabled" href="https://github.com/awaisqurashi/react-router-demo" tabindex="-1" aria-disabled="true">Disabled</a>
           </li>
         </ul>
         <form class="d-flex">
